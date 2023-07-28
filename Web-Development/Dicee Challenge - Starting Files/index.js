dd30b8de54f07d155fcdd2e89074eb664990e522
AdityaM2906 <adityameshram126@gmail.com>
2023-07-28 20:30:39 +0530

    28/07/2023 Done with DOM and the dice game started with the drums challenge.

--- Web-Development/Dicee Challenge - Starting Files/index.js
+++ Web-Development/Dicee Challenge - Starting Files/index.js
@@ -1,35 +1,31 @@
 var randomNumber1 = Math.floor(Math.random()*6) + 1;
 
-var randomDiceName1 = "dice"+randomNumber1+".png";
+var randomDice1 = "dice" + randomNumber1 + ".png";
 
-var randomImgSource1 = "images/"+randomDiceName;
+var randomsrc1 = "images/" + randomDice1;
 
-var image1 = document.querySelectorAll("img")[0];
+var dice1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src",randomImgSource1);
+dice1.setAttribute("src",randomsrc1);
+
 
 var randomNumber2 = Math.floor(Math.random()*6) + 1;
 
-var randomDiceName2 = "dice"+randomDiceName2+".png";
+var randomDice2 = "dice" + randomNumber2 + ".png";
 
-var randomImageSource2 = "images/"+randomDiceName2;
+var randomsrc2 = "images/" + randomDice2;
 
-var image2 = document.querySelectorAll("img")[1];
+var dice2 = document.querySelectorAll("img")[1];
 
-var image2 = document.setAttribute("src",randomImageSource2);
-
-if(randomNumber1>randomNumber2){
-    document.querySelector("h1").innerHTML="🚩 Play 1 Wins!"
-}
-else if(randomNumber1<randomNumber2){
-    document.querySelector("h1").innerHTML="🚩 Play 2 Wins!"
-}
-else{
-    document.querySelector("h1").innerHTML= "It's a draw!"
-
-}
+dice2.setAttribute("src",randomsrc2);
 
 
-
-
-
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  }
+  else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  }
+  else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
