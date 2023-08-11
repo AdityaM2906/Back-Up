a61c092e1ffc12409bd77e3bdb05c5fdb0dba854
AdityaM2906 <adityameshram126@gmail.com>
2023-08-11 21:15:22 +0530

    Completed Node.js and tried to install react

---
+++ Web-Development/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+//var generateName = require('sillyname');
+
+import generateName from "sillyname"
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}`)
+
+import superheroes from "superheroes";
+var myname = superheroes.random(); 
+console.log(`I am ${myname}!!`)
